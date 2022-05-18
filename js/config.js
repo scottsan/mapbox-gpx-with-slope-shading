174c1b75dc902ad233879af4ebdd1cab5225f3a2
Scott Mitchell <scott@mitchcast.com>
2022-05-18 17:00:31 -0400

    moved mapbox access token and style url to config js

---
+++ js/config.js
@@ -0,0 +1,19 @@
+﻿var config = (function () {
+
+    var t = {
+
+        get mapboxMapStyleUrl() {
+            return 'mapbox://styles/mitchellx/cl37syevj001514qnctrh8pql/draft';
+        },
+        get mapboxAccessToken() {
+            return 'pk.eyJ1IjoibWl0Y2hlbGx4IiwiYSI6ImNsMzRucWc2cDAwaWkzbHA5ZjF6YTB1b2cifQ.-ZHOTJY15FedqvH42WkwCg';
+        }
+        
+    }
+
+
+    return t;
+
+})();
+
+if (typeof module !== 'undefined') module.exports = config;
