f0f2763b81c215d614a4719aa418948087a2c6a2
Scott Mitchell <scott@mitchcast.com>
2022-05-19 07:38:20 -0400

    detecting and alerting when config updates are required

--- js/config.js
+++ js/config.js
@@ -2,15 +2,12 @@
 
     var t = {
 
-        get mapboxMapStyleUrl() {
-            return 'YOUR_MAPBOX_STYLE_URL_HERE';
-        },
-        get mapboxAccessToken() {
-            return 'YOUR_MAPBOX_ACCESS_TOKEN_HERE';
-        }
-        
-    }
+        mapboxAccessToken: 'YOUR_MAPBOX_ACCESS_TOKEN_HERE',
+        mapboxMapStyleUrl: 'YOUR_MAPBOX_STYLE_URL_HERE',
+        defaultMapCenterpoint: [-106.479390, 39.4684732],
+        defaultMapZoom: 14
 
+    }
 
     return t;
 
